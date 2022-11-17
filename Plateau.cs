7ec0e2c3ea645820b7ead241a971d3d31a1decf3
Sevane7 <sevane.bastian@gmail.com>
2022-11-17 17:52:40 +0100

    Create Plateau.cs

---
+++ Plateau.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mots_Meles
+{
+    internal class Plateau
+    {
+        private int difficult;   // niveau de difficulté
+        private bool existe;     // true si dans le dictionnaire
+        private int colonne;     // nombre de colonne par grille
+        private int ligne;       // nombre de ligne par grille
+        private int mot;         // nombre de mot par grille
+        public Plateau(int difficult, bool existe, int colonne, int ligne, int mot)
+        {
+            this.difficult = difficult;
+            this.existe = existe;
+            this.colonne = colonne;
+            this.ligne = ligne;
+            this.mot = mot;
+        }
+        public int Difficult { get { return this.difficult; } }
+        public bool Existe { get { return this.existe; } }
+        public int Colonne { get { return this.colonne; } }
+        public int Ligne { get { return this.ligne; } }
+        public int Mot { get { return this.mot; } }
+
+        //public bool Test_Plateau a besoin de public bool lettre_suivante.
+    }
+}
