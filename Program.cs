9796e15df3eb19476c6e179ab8a34c5d8362b675
mehdi913 <mehdiadel9192@gmail.com>
2022-11-30 16:08:05 +0100

    plato

--- Program.cs
+++ Program.cs
@@ -1,10 +1,42 @@
-﻿namespace Mots_Meles
+﻿using System;
+using System.IO;
+using System.IO.File;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mots_Meles
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.ReadKey();
+            Joueur joueur1 = new Joueur();
+            Joueur joueur2 = new Joueur();
+            Console.WriteLine("Entrez le nom du joueur 1");
+            this.nom.joueur1 = Console.ReadLine();
+            Console.WriteLine("Entrez le nom du joueur 2");
+            this.nom.joueur2 = Console.ReadLine();
+
+        }
+        public string[] GenererMots(string path)
+        {
+            if (this.difficulty == 1)
+            {
+
+            }
+        }
+        private void ReadFile(string filename)
+        {
+            try
+            {
+                string[] file_lines = File.ReadAllLines(filename);
+                tab_value = new string[file_lines.Length];
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
