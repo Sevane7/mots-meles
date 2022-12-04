eb059d33cd8c37c6753019ea3a7af65ed8ddea52
Sevane7 <sevane.bastian@gmail.com>
2022-12-04 23:21:45 +0100

    commentaires

--- Dictionnaire.cs
+++ Dictionnaire.cs
@@ -30,30 +30,52 @@ namespace Mots_Meles
             return n;
         }
         public string Langue { get { return this.langue; } }
-        /*public override string ToString()
-        {
-            string res = ($"Il a {this.nbr_mots} en {this.langue} de taille : ");
-            for(int i = 0; i<this.longueur.Length; i++) { res += this.longueur[i] + " ";  }
-            return res;
-        }*/
 
+
+        /// <summary>
+        /// Ca retourne quoi ca
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            string res = "";
+
+            return res;
+        }
+
+
+        /// <summary>
+        /// Affiche tous les mots du [][] mots
+        /// </summary>
         public void AfficherTousMots()
         {
             for (int i = 0; i < this.mots.Length; i++)
             {
                 for (int j = 0; j < this.mots[i].Length; j++)
                 {
-                    Console.WriteLine(this.mots[i][j]);
+                    Console.Write(this.mots[i][j] + " ");
+                    if (j == this.mots[i].Length - 1) { Console.Write(this.mots[i][j] + ".\n"); }
                 }
             }
         }
-        public bool RechDichRecursif(string[] tab, string mot, int index_deb, int index_fin, int milieu)   //tab depend de la longueur (si longueur = 9, tab contient tous les mots de longueur 9)
+
+
+        /// <summary>
+        ///Retourne l'index du mot cherché 
+        ///Dans le (mot.Length - 2) ème  tableau de [][] mots
+        /// </summary>
+        /// <param name="mot"></param>
+        /// <param name="index_deb"></param>
+        /// <param name="index_fin"></param>
+        /// <param name="milieu"></param>
+        /// <returns></returns>
+        public bool RechDichRecursif(string mot, int index_deb, int index_fin, int milieu)   //tab depend de la longueur (si longueur = 9, tab contient tous les mots de longueur 9)
         {
             int len = milieu + (index_fin - index_deb) / 2;
             if (len >= index_fin) { return false; }                                                        // le mots n'est pas dans tab
-            if (tab[len] == mot) { return true; }                                                          //condition d'arrêt si mot est dans le tableau 
-            if (mot.CompareTo(tab[len]) < 0) { return RechDichRecursif(tab, mot, index_deb, len, len); }
-            else { return RechDichRecursif(tab, mot, len + 1, index_fin, len + 1); }
+            if (this.mots[mot.Length - 2][len] == mot) { return true; }                                                          //condition d'arrêt si mot est dans le tableau 
+            if (mot.CompareTo(this.mots[mot.Length - 2][len]) < 0) { return RechDichRecursif(mot, index_deb, len, len); }
+            else { return RechDichRecursif(mot, len + 1, index_fin, len + 1); }
         }
         public void ReadFile(string filename)
         {
@@ -62,12 +84,13 @@ namespace Mots_Meles
             try
             {
                 var sr = new StreamReader(filename);
-                string fichier = sr.ReadToEnd();
+                string fichier = sr.ReadToEnd();                                 // Lis tous les éléments d'un fichier
                 string[] AllLines = new string[14];
                 for(int i = 0; i < 14; i++)
                 {
-                    string after = fichier.Split((i + 2).ToString() + "\n")[1]; // stocke tous les éléments après le split du premier nombre/chiffre
-                    AllLines[i] = after.Split((i + 3).ToString())[0];           // stocke tous les éléments du 2eme bloc avant le nombre/chiffre suivant
+                    string after = fichier.Split((i + 2).ToString() + "\n")[1]; // stocke tous les éléments après le split du premier nombre/chiffre 
+                    AllLines[i] = after.Split((i + 3).ToString())[0];           // stocke tous les éléments du 2eme bloc : avant le nombre/chiffre suivant
+                                                                                // [0] : premier bloc du .Split; [1] : deuxième
                 }
 
                 this.mots = new string[AllLines.Length][];
--- Excel.cs
+++ Excel.cs
@@ -14,16 +14,26 @@ namespace Mots_Meles
     {
         string path = "";
         Workbook wb;
-        Application excel;
-        Worksheet ws;
-        private int sheet;
-        public Excel(string path, Application excel, Workbook wb, Worksheet ws, int sheet)
+        Application excel;       // ça sert à quoi?
+        Worksheet ws;            
+        private int sheet;       // autant de sheets que de joueur
+        private int difficulty;
+        public Excel(string path, Application excel, Workbook wb, Worksheet ws, int sheet, int difficulty)
         {
             this.path = path;
             this.excel = excel;
             wb = excel.Workbooks.Open(path);
             ws = excel.Worksheets[sheet];
+            this.difficulty = difficulty;   
         }
+
+
+        /// <summary>
+        /// Get la valeur d'une cellule
+        /// </summary>
+        /// <param name="i"></param>
+        /// <param name="j"></param>
+        /// <returns></returns>
         public string ReadCell(int i, int j)
         {
             i++; // car tableau excel commence à 1
@@ -33,41 +43,87 @@ namespace Mots_Meles
             else
                 return "empty cell";
         }
-        public void WriteCelle(int i, int j, string value)
+
+
+        /// <summary>
+        /// set la valeur d'une cellule
+        /// </summary>
+        /// <param name="i"></param>
+        /// <param name="j"></param>
+        /// <param name="value"></param>
+        public void WriteCell(int i, int j, string value)
         {
             i++;
             j++;
             ws.Cells[i, j].Value2 = value;
         }
+
+
+        /// <summary>
+        /// Sauvegarde le WorkBook
+        /// </summary>
         public void Save()
         {
             wb.Save();
         }
+
+
+        /// <summary>
+        /// Sauvegarde Sous le WorkBook
+        /// </summary>
+        /// <param name="path"></param>
         public void SaveAs(string path)
         {
             wb.SaveAs(path);
         }
+
+
+        /// <summary>
+        /// instancie le WorkBook
+        /// </summary>
         public void CreateNewFile()
         {
             this.wb = excel.Workbooks.Add(XlWBATemplate.xlWBATWorksheet); 
         }
+
+
+        /// <summary>
+        /// Lis les valeurs de d'un seul WorkSheet 
+        /// Pour de l'Affichage console si besoin
+        /// </summary>
+        /// <param name="first_i"></param>
+        /// <param name="last_i"></param>
+        /// <param name="first_j"></param>
+        /// <param name="last_j"></param>
+        /// <returns></returns>
         public string [,] ReadRange(int first_i, int last_i, int first_j, int last_j)
         {
             Range range = (Range)ws.Range[ws.Cells[first_i, first_j], ws.Cells[last_i, last_j]];  // instancie un range (tableau Excel)
-            string [,] holder = range.Value;
+            string [,] interm = range.Value;
             string [,] returnstring = new string[last_i - first_i + 1, last_j - first_j + 1];
             for(int i = 1; i<=last_i - first_i; i++)
             {
                 for(int j = 1; j<=last_j - first_j; j++)
                 {
-                    returnstring[i - 1, j - 1] = holder[i, j].ToString();
+                    returnstring[i - 1, j - 1] = interm[i, j].ToString();
                 }
             }
             return returnstring;
         }
+
+
+        /// <summary>
+        /// Rempli un WorkSheet selon une matrice
+        /// On veut selon la difficulté et non une matrice
+        /// </summary>
+        /// <param name="first_i"></param>
+        /// <param name="last_i"></param>
+        /// <param name="first_j"></param>
+        /// <param name="last_j"></param>
+        /// <param name="writestring"></param>
         public void WriteString(int first_i, int last_i, int first_j, int last_j, string[,] writestring)
         {
-            Range range = (Range)ws.Range[ws.Cells[first_i, first_j], ws.Cells[last_i, last_j]];
+            Range range = ws.Range[ws.Cells[first_i, first_j], ws.Cells[last_i, last_j]];
             range.Value = writestring;
         }
     }
