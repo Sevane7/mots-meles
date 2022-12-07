e7686e116392e5c651191209afbbc64598d14334
mehdi913 <mehdiadel9192@gmail.com>
2022-12-07 13:41:02 +0100

    jeu

--- Jeu.cs
+++ Jeu.cs
@@ -3,10 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace Mots_Meles
 {
     internal class Jeu
     {
+        private Stopwatch timer;
+        private Joueur[] joueurs;
+
+        public Jeu(Stopwatch timer)
+        {
+            this.timer= timer;
+        }
+        public Stopwatch Timer
+        {
+            get {return this.timer;}
+            set {this.timer = value;}
+        }
+
+        public Stopwatch DebutTimer()
+        {
+         timer.Start();
+        }
+        public TimeSpan TempsEcoule()
+        {
+         timer.Stop();
+            TimeSpan temps = timer.Elapsed;
+            return TimeSpan;
+        }
     }
 }
